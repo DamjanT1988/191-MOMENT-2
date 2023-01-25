b76ee545526826a4fa7621c76cc9beaf4549ef09
DamjanT1988 <Damjan_t@hotmail.com>
2023-01-25 03:45:39 +0100

    Add project files.

---
+++ 191-MOMENT-2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using _191_MOMENT_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _191_MOMENT_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //--VIEWS AND ROUTES
+
+        //control route
+        [Route("/")]
+        //to page/view
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/add")]
+        //to page/view
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [Route("/edit")]
+        //to page/view
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        //--ERROR MANAGEMENT
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 191-MOMENT-2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _191_MOMENT_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
